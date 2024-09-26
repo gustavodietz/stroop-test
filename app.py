d75aa0fdaf9f3ee4b520b504cbc6a5611e4bdd4f
Gustavo Diez <gustavodiez@MacBook-Pro-de-Gustavo.local>
2024-09-26 10:48:20 +0200

    Fix missing import for os module

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template
+import os  # Asegúrate de tener esta línea para importar el módulo 'os'
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')  # Servir el archivo HTML con jsPsych
+    return render_template('index.html')
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
